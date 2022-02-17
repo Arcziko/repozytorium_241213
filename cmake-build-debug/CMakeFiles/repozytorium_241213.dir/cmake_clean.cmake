a3a4454f70c6d11a44e507ad17db4429c49a1e26
Arcziko <arkadiusz.kowalczyk.1992@gmail.com>
2022-02-17 11:54:46 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/repozytorium_241213.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/repozytorium_241213.dir/main.c.obj"
+  "librepozytorium_241213.dll.a"
+  "repozytorium_241213.exe"
+  "repozytorium_241213.exe.manifest"
+  "repozytorium_241213.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/repozytorium_241213.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/repozytorium_241213.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for repozytorium_241213.
+# This may be replaced when dependencies are built.
